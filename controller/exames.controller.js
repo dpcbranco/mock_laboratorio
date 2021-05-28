676d70353bbfec1a9ca807add50487d96f867ecb
Daniel P. C. Branco <danielpcbranco@gmail.com>
2021-05-27 22:13:47 -0300

    Adapta chamada à API de acordo com o .env

--- controller/exames.controller.js
+++ controller/exames.controller.js
@@ -1,6 +1,9 @@
-const {getExamesPorPaciente} = require('../services/exames.service');
+const exameService = process.env.LAB == 1 ? require('../services/examesLab1.service'):
+                     process.env.LAB == 2 ? require('../services/examesLab2.service'):
+                     process.env.LAB == 3 ? require('../services/examesLab3.service'):
+                     null;
 const getAllExames = async (req, res) => {
-    const exames = await getExamesPorPaciente(req.query.paciente);
+    const exames = await exameService.getExamesPorPaciente(req.query.paciente);
     if (exames.length === 0)
         return res.status(404).send({error: "Paciente sem exames"})
     return res.send(exames);
