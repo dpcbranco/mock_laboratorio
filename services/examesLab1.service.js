a89eab34ab804dfc833b03cf74caa709f0ec3d08
Daniel P. C. Branco <danielpcbranco@gmail.com>
2021-05-31 16:18:37 -0300

    Corrige nome do modelo

--- services/examesLab1.service.js
+++ services/examesLab1.service.js
@@ -1,4 +1,4 @@
-const Exame = require('../models/ExamesLab1');
+const Exame = require('../models/ExameLab1');
 const exameModel = require('mongoose').model('exameLab1', Exame, "exames");
 const getExamesPorPaciente = async (cpf) => {
     return await exameModel.find({
