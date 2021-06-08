801f0ded9e6e0d08737794310e413a4e4f197543
Daniel P. C. Branco <danielpcbranco@gmail.com>
2021-06-08 08:37:25 -0300

    Corrige busca pelo ID do exame.

--- services/tipoExames.service.js
+++ services/tipoExames.service.js
@@ -2,7 +2,7 @@ const TipoExame = require('../models/TipoExame');
 const tipoExameModel = require('mongoose').model('tipoExame', TipoExame, "tipoExames");
 
 const getTypeById = async (id) => {
-    return await tipoExameModel.findOne({ nome: Number.parseInt(id) });
+    return await tipoExameModel.findOne({ _id: id });
 };
 
 module.exports = {
