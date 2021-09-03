3e942d37efd520efd2fde3948ad4e1a4ab239a27
Lucas Stefenoni de Aquino Pereira <lucassap01@gmail.com>
2021-09-02 21:38:38 -0300

    Upload e Download de Arquivos dos labs 2 e 3

--- services/examesLab3.service.js
+++ services/examesLab3.service.js
@@ -1,15 +1,48 @@
 const Exame = require('../models/ExameLab3');
+const {connection, mongo} = require('mongoose');
+const ObjectId = require('mongoose').mongo.ObjectId;
 const exameModel = require('mongoose').model('exameLab3', Exame, "exames");
+
 const getExamesPorPaciente = async (cpf) => {
     return await exameModel.find({
         paciente: cpf
     })
 }
 
-const saveExamFile = (req, res) => {
-    
-}
+
+const getExameById = async (id) => {
+    return await exameModel.findById(id);
+};
+
+const fileUploadStream = async (fileName, mimeType) => {
+    const gfs = new mongo.GridFSBucket(connection.db, {bucketName: 'arquivos'});
+    return gfs.openUploadStream(
+        fileName,
+        { contentType: mimeType }
+    );
+
+};
+
+const fileDownloadStream = async (objectId) => {
+    const gfs = new mongo.GridFSBucket(connection.db, {bucketName: 'arquivos'});
+    return gfs.openDownloadStream(objectId);
+};
+
+const findFileById = async (id) => {
+    const gfs = new mongo.GridFSBucket(connection.db, {bucketName: 'arquivos'});
+    const result = await gfs.find({_id: ObjectId(id)}).toArray();
+    return result[0];
+};
+
+const updateExame = async (exame) => {
+    return await exame.save();
+};
 
 module.exports = {
-    getExamesPorPaciente, saveExamFile
+    getExamesPorPaciente,
+    getExameById,
+    fileUploadStream, 
+    updateExame,
+    fileDownloadStream,
+    findFileById
 };
